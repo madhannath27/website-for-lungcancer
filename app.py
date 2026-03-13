96b93e19ad3e41075378f07cd9a0a6b91775c721
madhannath27 <2303737721421013@ksrct.net>
2026-03-14 00:26:23 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, File, UploadFile
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import io
+
+app = FastAPI()
+
+model = tf.keras.models.load_model("D:\WEBSITE\BioLungNet_Model.h5")
+
+classes = ["Benign","Malignant","Normal"]
+
+def preprocess(img):
+    img = img.resize((224,224))
+    img = np.array(img)/255.0
+    img = np.expand_dims(img, axis=0)
+    return img
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents)).convert("RGB")
+    
+    img = preprocess(image)
+
+    prediction = model.predict(img)
+    pred_class = classes[np.argmax(prediction)]
+    confidence = float(np.max(prediction))
+
+    return {
+        "prediction": pred_class,
+        "confidence": confidence
+    }
